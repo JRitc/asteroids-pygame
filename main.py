e8a20182e0a2efed2c4e4eda92bc51a41a5629be
JRitc <Jake.Ritchie7@gmail.com>
2025-06-30 20:57:30 +0100

    added game window

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from constants import *
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Infinite game loop
+    while True:
+        screen.fill("black")
+
+        # Quits game if (x) button is pressed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+
+        pygame.display.flip()
+        # Infinite game loop end
+
+    
 
 if __name__ == "__main__":
     main()
