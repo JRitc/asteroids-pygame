41f95873ba8b295ecc9d3fcb976b074692a768a0
JRitc <Jake.Ritchie7@gmail.com>
2025-07-02 07:32:30 +0100

    added asteroid spawning and field

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     
-    
+    asteroid_group = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
+    Asteroid.containers = (updatable, drawable, asteroid_group)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
 
     dt = 0
 
